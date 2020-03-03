172fc25b2cda7f5bc5df67c9ffa3c12d24694fd3
farazinazrul <farazinazrul@yahoo.com>
2020-03-03 21:33:38 +0600

    added

---
+++ backup.js
@@ -0,0 +1,5 @@
+function add(num1, num2 = 10){
+    return num1+num2;
+    }
+    const total = add(10, 12);
+    console.log(total);
---
+++ templete.js
@@ -0,0 +1,22 @@
+const firstName = "Afroditi";
+const lastName = "Nidhi";
+const surName = "Farazi"
+const fullName = firstName + " " +lastName + " " +surName;
+const fullName2 = `${firstName} ${lastName} ${surName}`;
+console.log("before temp = ");
+console.log(fullName);
+console.log("after temp = ");
+console.log(fullName2);
+
+
+const multiline = "first line\n"
++"Second line\n"
++"third line\n"
+
+console.log(multiline);
+
+const multiline2 = `First line
+second line
+third line`;
+console.log(multiline2);
+
